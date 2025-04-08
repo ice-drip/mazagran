857b93a51cef41712a1aacd9a871f300c5076d31
rikka <admin@rikka.cc>
2025-04-08 11:30:37 +0800

    chore (dep): delete flex layout

--- packages/playground/src/app/app.module.ts
+++ packages/playground/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatInputModule } from "@angular/material/input";
 import { FormsModule } from "@angular/forms";
 import { MatIconModule } from "@angular/material/icon";
@@ -22,7 +21,6 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
     MatTooltipModule,
     MatSlideToggleModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
     MatInputModule,
     FormsModule,
     MatIconModule,
